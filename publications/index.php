d5c72fdbc4279c8949fda9f0c0eaf79cdef0d7d2
ichijimaosamu <ichijimaosamu@gmail.com>
2022-01-16 23:38:37 +0900

    論文・出版物ページ追加

---
+++ publications/index.php
@@ -0,0 +1,97 @@
+<?php include_once '../common/base_url.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width" />
+<link rel="icon" type="<?php echo $base_url . '/'; ?>favicon.ico" href="https://ashbi.kyoto-u.ac.jp/favicon.ico">
+<title>Amemori Group - Publications (日本語)</title>
+<meta name="description" content="Our Mission">
+<?php include_once '../common/common_head.php'; ?>
+</head>
+<body id="publications" class="lower" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+
+<div id="container">
+<!--container-->
+
+<?php include_once '../common/header.php'; ?>
+
+<div id="content">
+<!--content-->
+
+<main role="main">
+
+<article class="main">
+<!--main-->
+
+<div class="main__fv -lower">
+<div class="main__fv__inner -lower">
+<h1 class="main__fv__title -lower">論文・出版物</h1>
+<p class="main__fv__bg -lower"><img src="<?php echo $base_url . '/'; ?>img/publications_main_fv_bg.jpg" alt=""></p>
+</div>
+</div>
+
+<div class="main__inner">
+
+<ol class="breadcrumb-list" itemscope="" itemtype="https://schema.org/BreadcrumbList">
+<li class="breadcrumb-list__item breadcrumb-list__item--home" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><a href="" itemprop="item"><i class="fas fa-fw fa-home"></i><span itemprop="name">HOME</span></a><meta itemprop="position" content="1"></li>
+<li class="breadcrumb-list__item" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><a href="" itemprop="item"><span itemprop="name">論文・出版物</span></a><meta itemprop="position" content="2"></li>
+</ol>
+
+<ul class="chapter -publications">
+<li class="chapter__item -publications"><a href="#chapter1">2022</a></li>
+<li class="chapter__item -publications"><a href="#chapter2">2021</a></li>
+<li class="chapter__item -publications"><a href="#chapter3">2020</a></li>
+<li class="chapter__item -publications"><a href="#chapter4">2019</a></li>
+<li class="chapter__item -publications"><a href="#chapter5">2018</a></li>
+</ul>
+
+<section id="chapter1" class="publications__block block">
+<h2 class="block__title">2022</h2>
+
+<ol class="publications__list">
+<li class="publications__item">Isa K, Tokuoka K, Ikeda S, Karimi S, Kobayashi K, Sooksawate T, Isa T (2021) Amygdala underlies the environment-dependency of defense responses induced via superior colliculus. Frontiers in Neural Circuits, in press.</li>
+<li class="publications__item">Kasai M, Isa T (2021) Effects of light isoflurane anesthesia on organization of direction and orientation selectivity in the superficial layer of the mouse superior colliculus, Journal of Neuroscience, JN-RM-1196-21<span class="publications__item__link"><a href="#" target="_blank" rel="noopener">DOI</a></span><span class="publications__item__link"><a href="#" target="_blank" rel="noopener">PUBMED</a></span></li>
+<li class="publications__item">Kato R, Zeghbib A, Redgrave P, Isa T (2021) Visual instrumental learning in blindsight monkeys. Scientific Reports 11, 14819.<span class="publications__item__link"><a href="#" target="_blank" rel="noopener">DOI</a></span></li>
+<li class="publications__item">Eberle H, Hayashi Y, Kurazume R, Takei T, An Q. Modeling of hyper-adaptability: from motor coordination to rehabilitation. Adv Robotics. 2021;1–16.<span class="publications__item__link"><a href="#" target="_blank" rel="noopener">DOI</a></span></li>
+<li class="publications__item">Isa T, Yoshida M (2021) Neural mechanism of blindsight in a macaque model. Neuroscience, (Forefront review), 469: 138-161.<span class="publications__item__link"><a href="#" target="_blank" rel="noopener">DOI</a></span></li>
+<li class="publications__item">Isa T, Marquez-Legorreta E, Grillner S, Scott EK (2021) The tectum/superior colliculus as the vertebrate solution for spatial sensory integration and action. Current Biology (review), VOLUME 31, ISSUE 11, PR741-R762<span class="publications__item__link"><a href="#" target="_blank" rel="noopener">DOI</a></span></li>
+<li class="publications__item">Liao Y-P, Urayama S, Isa T, Fukuyama H* (2021) Optimal Model Mapping for Intravoxal Incoherent Motion MRI. Frontiers in Human Neuroscience, 15:617152<span class="publications__item__link"><a href="#" target="_blank" rel="noopener">DOI</a></span></li>
+<li class="publications__item">Okada T*, Kuribayashi H, Kaiser LG, Urushibata Y, Salibi N, Seethamraju RT, Ahn S, Thuy DHD, Fujimoto K, Isa T (2021) Repeatability of proton magnetic resonance spectroscopy of the brain at 7 T: effect of scan time on semi-localized by adiabatic selective refocusing and short-echo time stimulated echo acquisition mode scans and their comparison.Quant Imaging Med Surg. 11:9-20.<span class="publications__item__link"><a href="#" target="_blank" rel="noopener">DOI</a></span><span class="publications__item__link"><a href="#" target="_blank" rel="noopener">PUBMED</a></span></li>
+</ol>
+</section>
+
+<section id="chapter2" class="publications__block block">
+<h2 class="block__title">2021</h2>
+
+<ol class="publications__list">
+<li class="publications__item">Isa K, Tokuoka K, Ikeda S, Karimi S, Kobayashi K, Sooksawate T, Isa T (2021) Amygdala underlies the environment-dependency of defense responses induced via superior colliculus. Frontiers in Neural Circuits, in press.</li>
+<li class="publications__item">Kasai M, Isa T (2021) Effects of light isoflurane anesthesia on organization of direction and orientation selectivity in the superficial layer of the mouse superior colliculus, Journal of Neuroscience, JN-RM-1196-21<span class="publications__item__link"><a href="#" target="_blank" rel="noopener">DOI</a></span><span class="publications__item__link"><a href="#" target="_blank" rel="noopener">PUBMED</a></span></li>
+</ol>
+</section>
+
+</div>
+
+<!--/main-->
+</article>
+
+</main>
+
+<!--/content-->
+</div>
+
+<?php include_once '../common/footer.php'; ?>
+
+<!--/container-->
+</div>
+
+<?php include_once '../common/common_script.php'; ?>
+
+<?php if(false) { ?>
+<!--sp_menu-->
+<?php include_once '../common/sp_menu.php'; ?>
+<!--/sp_menu-->
+<?php } ?>
+
+</body>
+</html>
