1cc95a2f397439e5caa50a5c22bd2ba927893119
ichijimaosamu <ichijimaosamu@gmail.com>
2022-01-20 07:37:28 +0900

    ニュース一覧ページ追加

---
+++ news/index.php
@@ -0,0 +1,245 @@
+<?php include_once '../common/base_url.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width" />
+<link rel="icon" type="<?php echo $base_url . '/'; ?>favicon.ico" href="https://ashbi.kyoto-u.ac.jp/favicon.ico">
+<title>Amemori Group - News (日本語)</title>
+<meta name="description" content="Our Mission">
+<?php include_once '../common/common_head.php'; ?>
+</head>
+<body id="news" class="lower" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+
+<div id="container">
+<!--container-->
+
+<?php include_once '../common/header.php'; ?>
+
+<div id="content">
+<!--content-->
+
+<main role="main">
+
+<article class="main">
+<!--main-->
+
+<div class="main__fv -lower">
+<div class="main__fv__inner -lower">
+<h1 class="main__fv__title -lower">ニュース</h1>
+<p class="main__fv__bg -lower"><img src="<?php echo $base_url . '/'; ?>img/news_main_fv_bg.jpg" alt=""></p>
+</div>
+</div>
+
+<div class="main__inner">
+
+<ol class="breadcrumb-list" itemscope="" itemtype="https://schema.org/BreadcrumbList">
+<li class="breadcrumb-list__item breadcrumb-list__item--home" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><a href="" itemprop="item"><i class="fas fa-fw fa-home"></i><span itemprop="name">HOME</span></a><meta itemprop="position" content="1"></li>
+<li class="breadcrumb-list__item" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><a href="" itemprop="item"><span itemprop="name">ニュース</span></a><meta itemprop="position" content="2"></li>
+</ol>
+
+<ul class="chapter">
+<li class="chapter__item"><a href="">All</a></li>
+<li class="chapter__item"><a href="">News</a></li>
+<li class="chapter__item"><a href="">Blog</a></li>
+<li class="chapter__item"><a href="">Events</a></li>
+</ul>
+
+<ol class="news__list">
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="<?php echo $base_url . '/'; ?>img/news_item_pic1.jpg" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">Principles of Neural Science第６版で研究内容が引用されました</span>
+</dd>
+<dd class="news__item__cate">News</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="<?php echo $base_url . '/'; ?>img/news_item_pic2.jpg" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">Principles of Neural Science第６版で研究内容が引用されました</span>
+</dd>
+<dd class="news__item__cate">News</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="<?php echo $base_url . '/'; ?>img/news_item_pic3.jpg" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">Principles of Neural Science第６版で研究内容が引用されました</span>
+</dd>
+<dd class="news__item__cate">Blog</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="<?php echo $base_url . '/'; ?>img/news_item_pic4.jpg" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">Principles of Neural Science第６版で研究内容が引用されました</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+<li class="news__item"><a href="#">
+<dl>
+<dt class="news__item__term"><img src="https://placehold.jp/200x130.png" alt=""></dt>
+<dd class="news__item__description">
+<span class="news__item__date">2022.1.10</span>
+<span class="news__item__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</span>
+</dd>
+<dd class="news__item__cate">Events</dd>
+</dl>
+</a></li>
+</ol>
+
+<ol class="pagenation">
+<li class="pagenation__item -prev"><a href=""><i class="fas fa-angle-double-left"></i></a></li>
+<li class="pagenation__item -is-active">1</li>
+<li class="pagenation__item"><a href="">2</a></li>
+<li class="pagenation__item"><a href="">3</a></li>
+<li class="pagenation__item"><a href="">4</a></li>
+<li class="pagenation__item"><a href="">5</a></li>
+<li class="pagenation__item -next"><a href=""><i class="fas fa-angle-double-right"></i></a></li>
+</ol>
+
+</div>
+
+<!--/main-->
+</article>
+
+</main>
+
+<!--/content-->
+</div>
+
+<?php include_once '../common/footer.php'; ?>
+
+<!--/container-->
+</div>
+
+<?php include_once '../common/common_script.php'; ?>
+
+<?php if(false) { ?>
+<!--sp_menu-->
+<?php include_once '../common/sp_menu.php'; ?>
+<!--/sp_menu-->
+<?php } ?>
+
+</body>
+</html>
