a09507f32f4eebb73d5762c9c587f374d22d768a
ichijimaosamu <ichijimaosamu@gmail.com>
2022-01-25 07:55:49 +0900

    メンバーページ追加（ソースのみ）

---
+++ member/index.php
@@ -0,0 +1,166 @@
+<?php include_once '../common/base_url.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width" />
+<link rel="icon" type="<?php echo $base_url . '/'; ?>favicon.ico" href="https://ashbi.kyoto-u.ac.jp/favicon.ico">
+<title>Amemori Group - Member (日本語)</title>
+<meta name="description" content="Our Mission">
+<?php include_once '../common/common_head.php'; ?>
+</head>
+<body id="member" class="lower" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+
+<div id="container">
+<!--container-->
+
+<?php include_once '../common/header.php'; ?>
+
+<div id="content">
+<!--content-->
+
+<main role="main">
+
+<article class="main">
+<!--main-->
+
+<div class="main__fv -lower">
+<div class="main__fv__inner -lower">
+<h1 class="main__fv__title -lower">メンバー</h1>
+<p class="main__fv__bg -lower"><img src="<?php echo $base_url . '/'; ?>img/member_main_fv_bg.jpg" alt=""></p>
+</div>
+</div>
+
+<div class="main__inner">
+
+<ol class="breadcrumb-list" itemscope="" itemtype="https://schema.org/BreadcrumbList">
+<li class="breadcrumb-list__item breadcrumb-list__item--home" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><a href="" itemprop="item"><i class="fas fa-fw fa-home"></i><span itemprop="name">HOME</span></a><meta itemprop="position" content="1"></li>
+<li class="breadcrumb-list__item" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><a href="" itemprop="item"><span itemprop="name">メンバー</span></a><meta itemprop="position" content="2"></li>
+</ol>
+
+<ul class="chapter">
+<li class="chapter__item"><a href="#chapter1">教員</a></li>
+<li class="chapter__item"><a href="#chapter2">研究員</a></li>
+<li class="chapter__item"><a href="#chapter3">大学院生</a></li>
+</ul>
+
+<section id="chapter1" class="member__group group">
+<h2 class="group__title">教員</h2>
+
+<div class="member__block">
+
+<dl class="member__info">
+<dt class="member__info__pic"><img src="<?php echo $base_url . '/'; ?>img/member_info_pic_amemorikenichi.jpg" alt="雨森 賢一"></dt>
+<dd class="member__info__description">
+<p class="member__info__name">雨森 賢一<span class="en">Ken-ichi Amemori</span></p>
+<p class="member__info__degree">助教授 / PI　Associate Professor</p>
+<p class="member__info__introduction">計算論的神経科学の分野で研究を始めた後、現在では霊長類を用いた神経生理学の分野に従事しています。マカクザルの前帯状皮質、前頭前皮質、大脳基底核の研究をやってきました。他には、情動、意思決定、価値判断に興味があります。Researchのページで詳しい研究内容を説明しています。</p>
+</dd>
+</dl>
+
+<dl class="member__history">
+<dt class="member__history__term">略歴</dt>
+<dd class="member__history__description">京都⼤学卒業後、奈良先端科学技術⼤学院⼤学で博⼠号取得（2001年）。北海道⼤学医学部助⼿を経て、⽶国マサチューセッツ⼯科⼤学でポストドクトラルアソシエイト・フェロー（2005年）。その後リサーチサイエンティスト（2009年）を経て、2017年に京都⼤学⽩眉センター・霊⻑類研究所特定准教授となり現在に⾄る。</dd>
+</dl>
+
+<div class="member__footer">
+<p class="member__footer__mail">XXXX.XXXX.XX★kyoto-u.ac.jp<span class="member__footer__mail__notice">※★を@マークに変更して送信ください</span></p>
+<p class="member__footer__website"><a href="" target="_blank" rel="noopener">ウェブサイト</a></p>
+<ul class="member__footer__sns">
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_facebook.png" alt="Facebook"></a></li>
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_twitter.png" alt="Twitter"></a></li>
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_linkedin.png" alt="LinkedIn"></a></li>
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_researchgate.png" alt="Researchgate"></a></li>
+</ul>
+</div>
+
+</div>
+
+<div class="member__block">
+
+<dl class="member__info">
+<dt class="member__info__pic"><img src="<?php echo $base_url . '/'; ?>img/member_info_pic_saitoharumi.jpg" alt="齊藤 治美"></dt>
+<dd class="member__info__description">
+<p class="member__info__name">齊藤 治美<span class="en">Harumi Saito</span></p>
+<p class="member__info__degree">助教 / Associate Professor</p>
+<p class="member__info__introduction">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+</dd>
+</dl>
+
+<dl class="member__history">
+<dt class="member__history__term">略歴</dt>
+<dd class="member__history__description">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</dd>
+</dl>
+
+<div class="member__footer">
+<p class="member__footer__mail">XXXX.XXXX.XX★kyoto-u.ac.jp<span class="member__footer__mail__notice">※★を@マークに変更して送信ください</span></p>
+<p class="member__footer__website"><a href="" target="_blank" rel="noopener">ウェブサイト</a></p>
+<ul class="member__footer__sns">
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_facebook.png" alt="Facebook"></a></li>
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_twitter.png" alt="Twitter"></a></li>
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_linkedin.png" alt="LinkedIn"></a></li>
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_researchgate.png" alt="Researchgate"></a></li>
+</ul>
+</div>
+
+</div>
+</section>
+
+<section id="chapter2" class="member__group group">
+<h2 class="group__title">研究員</h2>
+
+<div class="member__block">
+
+<dl class="member__info">
+<dt class="member__info__pic"><img src="https://placehold.jp/220x260.png" alt=""></dt>
+<dd class="member__info__description">
+<p class="member__info__name">雨森 智子<span class="en">Tomoko Amemori</span></p>
+<p class="member__info__degree">研究員 / Researcher</p>
+<p class="member__info__introduction">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+</dd>
+</dl>
+
+<dl class="member__history">
+<dt class="member__history__term">略歴</dt>
+<dd class="member__history__description">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</dd>
+</dl>
+
+<div class="member__footer">
+<p class="member__footer__mail">XXXX.XXXX.XX★kyoto-u.ac.jp<span class="member__footer__mail__notice">※★を@マークに変更して送信ください</span></p>
+<p class="member__footer__website"><a href="" target="_blank" rel="noopener">ウェブサイト</a></p>
+<ul class="member__footer__sns">
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_facebook.png" alt="Facebook"></a></li>
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_twitter.png" alt="Twitter"></a></li>
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_linkedin.png" alt="LinkedIn"></a></li>
+<li class="member__footer__sns__item"><a href="" target="_blank" rel="noopener"><img src="<?php echo $base_url . '/'; ?>img/member_footer_sns_item_researchgate.png" alt="Researchgate"></a></li>
+</ul>
+</div>
+
+</div>
+</section>
+
+</div>
+
+<!--/main-->
+</article>
+
+</main>
+
+<!--/content-->
+</div>
+
+<?php include_once '../common/footer.php'; ?>
+
+<!--/container-->
+</div>
+
+<?php include_once '../common/common_script.php'; ?>
+
+<?php if(false) { ?>
+<!--sp_menu-->
+<?php include_once '../common/sp_menu.php'; ?>
+<!--/sp_menu-->
+<?php } ?>
+
+</body>
+</html>
