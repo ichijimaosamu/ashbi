f5d12e97ffee8a047570e9be34b717245372d267
ichijimaosamu <ichijimaosamu@gmail.com>
2022-01-27 00:49:48 +0900

    技術開発ページ　テンプレート再修正

--- technology/index.php
+++ technology/index.php
@@ -39,44 +39,54 @@
 <li class="breadcrumb-list__item" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem"><a href="" itemprop="item"><span itemprop="name">技術開発</span></a><meta itemprop="position" content="2"></li>
 </ol>
 
-<section class="technology__about block">
+<section class="block">
 <h2 class="block__title">私たちの技術開発について</h2>
 
-<div class="technology__about__box">
-<p class="technology__about__box__title">タンパク質解析前処理</p>
-<p class="technology__about__box__text">培養した細胞や組織からタンパク質を抽出し、主に酵素を用いてタンパク質を消化します（タンパク質をペプチドにします）。</p>
-</div>
-
-<div class="technology__about__box">
-<p class="technology__about__box__title">消化したタンパク質の測定</p>
-<p class="technology__about__box__text">液体クロマトグラフィーで消化したタンパク質を分離しながら、質量分析計で測定します。この時、エレクトロスプレイイオン化法と呼ばれる手法を用いてペプチドをイオン化しています。この手法の開発者のジョン・フェン博士は2002年にノーベル化学賞を受賞されました。</p>
-</div>
-
-<div class="technology__about__box">
-<p class="technology__about__box__title">タンパク質の解析と検証</p>
-<p class="technology__about__box__text">質量分析計でとったデータをもとに、MascotやProteinPilotなどのデータベース解析ソフトを用いてタンパク質を同定します。この時、定量精度を高めたり、より良い解析結果を得るために、様々な解析ツールを使用・開発します。さらに、ウェスタンブロッティングや免疫染色などを行ってデータの検証をします。</p>
-</div>
-
-<p class="technology__about__pic"><img src="<?php echo $base_url . '/'; ?>img/technology_about_pic.jpg" alt=""></p>
+<p>
+●タンパク質解析前処理<br>
+培養した細胞や組織からタンパク質を抽出し、主に酵素を用いてタンパク質を消化します（タンパク質をペプチドにします）。<br>
+<br>
+●消化したタンパク質の測定<br>
+液体クロマトグラフィーで消化したタンパク質を分離しながら、質量分析計で測定します。この時、エレクトロスプレイイオン化法と呼ばれる手法を用いてペプチドをイオン化しています。この手法の開発者のジョン・フェン博士は2002年にノーベル化学賞を受賞されました。<br>
+<br>
+●タンパク質の解析と検証<br>
+質量分析計でとったデータをもとに、MascotやProteinPilotなどのデータベース解析ソフトを用いてタンパク質を同定します。この時、定量精度を高めたり、より良い解析結果を得るために、様々な解析ツールを使用・開発します。さらに、ウェスタンブロッティングや免疫染色などを行ってデータの検証をします。<br>
+<span class="pic"><img src="<?php echo $base_url . '/'; ?>img/technology_about_pic.jpg" alt=""></span>
+</p>
 
 </section>
 
-<section class="technology__equipments block">
+<section class="block">
 <h2 class="block__title">実験装置</h2>
 
 <ul class="photo__list">
-<li class="photo__item"><a href="<?php echo $base_url . '/'; ?>img/photo_item1_b.jpg" data-lightbox="photo" data-title=""><img src="<?php echo $base_url . '/'; ?>img/photo_item1_m.jpg" alt=""></a></li>
-<li class="photo__item"><a href="<?php echo $base_url . '/'; ?>img/photo_item2_b.jpg" data-lightbox="photo" data-title=""><img src="<?php echo $base_url . '/'; ?>img/photo_item2_m.jpg" alt=""></a></li>
-<li class="photo__item"><a href="<?php echo $base_url . '/'; ?>img/photo_item3_b.jpg" data-lightbox="photo" data-title=""><img src="<?php echo $base_url . '/'; ?>img/photo_item3_m.jpg" alt=""></a></li>
-<li class="photo__item"><a href="https://placehold.jp/612x400.png" data-lightbox="photo" data-title=""><img src="https://placehold.jp/306x200.png" alt=""></a></li>
-<li class="photo__item"><a href="https://placehold.jp/612x400.png" data-lightbox="photo" data-title=""><img src="https://placehold.jp/306x200.png" alt=""></a></li>
-<li class="photo__item"><a href="https://placehold.jp/612x400.png" data-lightbox="photo" data-title=""><img src="https://placehold.jp/306x200.png" alt=""></a></li>
-<li class="photo__item"><a href="https://placehold.jp/612x400.png" data-lightbox="photo" data-title=""><img src="https://placehold.jp/306x200.png" alt=""></a></li>
-<li class="photo__item"><a href="https://placehold.jp/612x400.png" data-lightbox="photo" data-title=""><img src="https://placehold.jp/306x200.png" alt=""></a></li>
-<li class="photo__item"><a href="https://placehold.jp/612x400.png" data-lightbox="photo" data-title=""><img src="https://placehold.jp/306x200.png" alt=""></a></li>
-<li class="photo__item"><a href="https://placehold.jp/612x400.png" data-lightbox="photo" data-title=""><img src="https://placehold.jp/306x200.png" alt=""></a></li>
-<li class="photo__item"><a href="https://placehold.jp/612x400.png" data-lightbox="photo" data-title=""><img src="https://placehold.jp/306x200.png" alt=""></a></li>
-<li class="photo__item"><a href="https://placehold.jp/612x400.png" data-lightbox="photo" data-title=""><img src="https://placehold.jp/306x200.png" alt=""></a></li>
+<li class="photo__item">
+<a href="<?php echo $base_url . '/'; ?>img/photo_item1_b.jpg" data-lightbox="photo" data-title="">
+<img src="<?php echo $base_url . '/'; ?>img/photo_item1_m.jpg" alt="">
+<dl>
+<dt class="photo__item__term">nanoLC-MS/MSシステム (TripleTOF 5600 (Sciex))</dt>
+<dd class="photo__item__description">ナノ液体クロマトグラフィー(Dionex)とオートサンプラー(PAL)に質量分析計をつなげています。</dd>
+</dl>
+</a>
+</li>
+<li class="photo__item">
+<a href="<?php echo $base_url . '/'; ?>img/photo_item2_b.jpg" data-lightbox="photo" data-title="">
+<img src="<?php echo $base_url . '/'; ?>img/photo_item2_m.jpg" alt="">
+<dl>
+<dt class="photo__item__term">プラー-顕微鏡-金コート用機器</dt>
+<dd class="photo__item__description">内径75-100umのガラス配管に熱を当てながら引っ張り、スプレイヤーチップを作る機械です。さらに、それを金コートして伝導性を高めます。</dd>
+</dl>
+</a>
+</li>
+<li class="photo__item">
+<a href="<?php echo $base_url . '/'; ?>img/photo_item3_b.jpg" data-lightbox="photo" data-title="">
+<img src="<?php echo $base_url . '/'; ?>img/photo_item3_m.jpg" alt="">
+<dl>
+<dt class="photo__item__term">カラムローダー</dt>
+<dd class="photo__item__description">内径75-100umのガラス配管に3um程度のC18粒子を詰めて、分析用のカラムを作ります。</dd>
+</dl>
+</a>
+</li>
 </ul>
 </section>
 
