61b97820babde7029ede4da31ee9d56bcbd20e74
ichijimaosamu <ichijimaosamu@gmail.com>
2022-01-27 00:50:18 +0900

    スマホメニュー追加（ソースのみ）

--- common/sp_menu.php
+++ common/sp_menu.php
@@ -4,15 +4,15 @@
 </div>
 <div id="drawernav" class="hidden-md hidden-lg hmenu">
 <ul class="drawernav__list">
-<li class="drawernav__item"><a href="<?php echo $base_url . '/'; ?>">HOME</a></li>
-<li class="drawernav__item"><a href="https://www.bmc-net.jp/ec/" target="_blank">DVD・動画販売</a></li>
-<li class="drawernav__item"><a href="<?php echo $base_url . '/'; ?>service/">サービスについて</a></li>
-<li class="drawernav__item"><a href="<?php echo $base_url . '/'; ?>company/">会社案内</a></li>
-</ul>
-<ul class="header__contact__list drawernav__contact__list">
-<li class="header__contact__item drawernav__contact__item -tel"><a href="tel:03-3569-0968" class="-tel">03-3569-0968</a></li>
-<li class="header__contact__item drawernav__contact__item -fax"><a href="tel:03-6215-9218" class="-fax">03-6215-9218</a></li>
-<li class="header__contact__item drawernav__contact__item -inquiry"><a href="<?php echo $base_url . '/'; ?>contact/">お問い合わせ</a></li>
+<li class="drawernav__item"><a href="">ホーム</a></li>
+<li class="drawernav__item"><a href="">メンバー</a></li>
+<li class="drawernav__item"><a href="">研究概要</a></li>
+<li class="drawernav__item"><a href="">論文・出版物</a></li>
+<li class="drawernav__item"><a href="">ニュース</a></li>
+<li class="drawernav__item"><a href="">技術開発</a></li>
+<li class="drawernav__item"><a href="">リンク</a></li>
+<li class="drawernav__item"><a href="">ENGLISH SITE</a></li>
+<li class="drawernav__item"><a href="">お問い合わせ</a></li>
 </ul>
 </div>
 <script src="<?php echo $base_url . '/'; ?>js/humbergermenu_sp.js"></script>
