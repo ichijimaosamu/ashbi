8afae06b4bf65f86cbd03b8fdbd387502f2be68e
ichijimaosamu <ichijimaosamu@gmail.com>
2022-01-27 17:24:29 +0900

    common.js　最新の状態に。

--- js/common.js
+++ js/common.js
@@ -97,36 +97,8 @@ $(function() {
 			rightNav.fadeOut();
 		}
 	});
+});
 
-	/*スムーススクロール*/
-	$(document).ready(function(){
-
-		var urlHash = location.hash;
-
-		if(urlHash) {
-
-			$('body,html').stop().scrollTop(0);
-			setTimeout(function () {
-
-				scrollToAnker(urlHash) ;
-			}, 100);
-		}
-
-
-		$('a[href^="#"]').click(function() {
-
-			var href= $(this).attr("href");
-
-			var hash = href == "#" || href == "" ? 'html' : href;
-
-			scrollToAnker(hash);
-			return false;
-		});
-
-		function scrollToAnker(hash) {
-			var target = $(hash);
-			var position = target.offset().top - $("#header").outerHeight();
-			$('body,html').stop().animate({scrollTop:position,}, 500);
-		}
-	})
+$(function(){
+	$('.alert ').hide();
 });
