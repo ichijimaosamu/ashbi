8054ac619a50e5beda9e09c532ac629d013bd0e1
ichijimaosamu <ichijimaosamu@gmail.com>
2022-01-28 07:13:19 +0900

    お問い合わせページ　スタイル追加

--- contact/index.php
+++ contact/index.php
@@ -16,7 +16,7 @@
 
 <?php include_once '../common/header.php'; ?>
 
-<div id="content">
+<div id="content" class="-contact">
 <!--content-->
 
 <main role="main">
@@ -45,6 +45,8 @@
 
 <p class="contact__box__notice"><span class="required">必須</span>は必須入力です。</p>
 
+<div role="form" class="wpcf7" id="wpcf7-f186-p9-o1" lang="ja" dir="ltr">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
 <form action="/ashbi_temp/contact/#wpcf7-f186-p9-o1" method="post" class="wpcf7-form init" novalidate="novalidate" data-status="init">
 <div style="display: none;">
 <input type="hidden" name="_wpcf7" value="186">
@@ -54,22 +56,16 @@
 <input type="hidden" name="_wpcf7_container_post" value="9">
 <input type="hidden" name="_wpcf7_posted_data_hash" value="">
 </div>
-<p><label><span class="contact__form__title">お名前<span class="required">必須</span></span>
-<span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span></label></p>
-<p><label><span class="contact__form__title">ご所属<span class="required">必須</span></span>
-<span class="wpcf7-form-control-wrap your-belongs"><input type="text" name="your-belongs" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span></label></p>
-<p><label><span class="contact__form__title">ご連絡先メールアドレス<span class="required">必須</span></span>
-<span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"></span></label></p>
-<p><label><span class="contact__form__title">ご連絡先メールアドレス［確認用］<span class="required">必須</span></span>
-<span class="wpcf7-form-control-wrap your-email-confirm"><input type="email" name="your-email-confirm" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"></span></label></p>
-<p><label><span class="contact__form__title">件名<span class="required">必須</span></span>
-<span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span></label></p>
-<p><label><span class="contact__form__title">内容<span class="required">必須</span></span>
-<span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-required="true" aria-invalid="false"></textarea></span></label></p>
-<p><label><input type="checkbox" class="contact__form__check">内容をご確認のうえ、こちらにチェックを入れてください。</label></p>
-<p><input type="submit" value="確認画面へ" class="wpcf7-form-control has-spinner wpcf7-submit"><span class="wpcf7-spinner"></span></p>
-<div class="wpcf7-response-output" aria-hidden="true"></div>
-</form>
+<p><label class="contact__form__flex"><span class="contact__form__title">お名前<span class="required">必須</span></span><span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span></label></p>
+<p><label class="contact__form__flex"><span class="contact__form__title">ご所属<span class="required">必須</span></span><span class="wpcf7-form-control-wrap text-239"><input type="text" name="text-239" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span></label></p>
+<p><label class="contact__form__flex"><span class="contact__form__title">ご連絡先メールアドレス<span class="required">必須</span></span><span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"></span> </label></p>
+<p><label class="contact__form__flex"><span class="contact__form__title">ご連絡先メールアドレス<span class="required">必須</span></span><span class="wpcf7-form-control-wrap your-email_confirm"><input type="email" name="your-email_confirm" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"></span> </label></p>
+<p><label class="contact__form__flex"><span class="contact__form__title">件名<span class="required">必須</span></span><span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span> </label></p>
+<p><label class="contact__form__flex"><span class="contact__form__title">内容<span class="required">必須</span></span><span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></textarea></span> </label></p>
+<p><label><span class="wpcf7-form-control-wrap checkbox-4"><span class="wpcf7-form-control wpcf7-checkbox wpcf7-validates-as-required"><span class="wpcf7-list-item first last"><label><input type="checkbox" name="checkbox-4[]" value="内容をご確認のうえ、こちらにチェックを入れてください。"><span class="wpcf7-list-item-label">内容をご確認のうえ、こちらにチェックを入れてください。</span></label></span></span></span></label></p>
+<p><input type="submit" value="送  信" class="wpcf7-form-control has-spinner wpcf7-submit"><span class="wpcf7-spinner"></span></p>
+<div class="wpcf7-response-output" aria-hidden="true"></div></form>
+</div>
 
 </div>
 </section>
@@ -80,8 +76,8 @@
 <div class="access__flex">
 <dl>
 <dt class="access__term">住所</dt>
-<dd class="access__description">〒606-8507
-京都市左京区吉田近衛町
+<dd class="access__description">〒606-8507<br>
+京都市左京区吉田近衛町<br>
 京都大学医学部構内　医学部B棟</dd>
 <dt class="access__term">アクセス方法</dt>
 <dd class="access__description">ダミーテキストダミーテキストダミーテキスト<br>
